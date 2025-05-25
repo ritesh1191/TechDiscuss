d672dff6e6b0f228bd32df0836e0d8d4add917e6
Ritesh Thakare <riteshthakare1191@gmail.com>
2025-05-25 14:54:19 +0530

    updated build

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:5001";
-export const IF = "http://localhost:5001/images/";
+export const URL = "https://techdiscuss.onrender.com";
+export const IF = "https://techdiscuss.onrender.com/images/";
